b2d15bd88bf536f3128ddd3cbf635615344bd1d3
aslam <aslam13071999@gmail.com>
2022-08-20 15:42:15 +0530

    Have added themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,24 @@ module.exports = {
   mode: "jit",
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+
   ],
   theme: {
+    colors:{
+      'light':{
+        'primary': '#ffffff',
+        'secondary':'#111827',
+        'br-color':'#a3a3a3',
+        'lt-gray':'#e5e5e5',
+        'blue':'#1e40af'
+      },
+      'dark':{
+        'primary':'#111827',
+        'secondary':'#ffffff'
+      }
+      },
+
+
     extend: {
     }
   },
